3ca1ffac37f242f7d0dfea80f378e0b52af706e4
fatih <fatihenaz@gmail.com>
2021-09-01 07:53:11 +0300

    import Lİnk, added Link to instead of <a href>

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Home from "./components/Home"
 import About from "./components/About";
 import Contact from "./components/Contact";
 import NotFound from "./components/NotFound";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 
 
@@ -15,9 +15,9 @@ function App() {
         <nav>
           <h1>CLARUSWAY</h1>
           <ul>
-            <li><a href="/">Home</a></li>
-            <li><a href="/about">About</a></li>
-            <li><a href="/contact">Contact</a></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
           </ul>
         </nav>
         <Switch>
