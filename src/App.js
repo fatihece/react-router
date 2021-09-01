a669cce3e0baee659ee8c9df07be1025594927ce
fatih <fatihenaz@gmail.com>
2021-09-01 08:03:39 +0300

    Detail js added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home"
 import About from "./components/About";
 import Contact from "./components/Contact";
 import NotFound from "./components/NotFound";
+import Details from "./components/Details";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 
 
@@ -18,12 +19,15 @@ function App() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/about">About</Link></li>
             <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/details/abc">Details</Link></li>
+            
           </ul>
         </nav>
         <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
+        <Route path="/details" component={Details} />
         <Route component={NotFound} />
         </Switch>
       </div>
---
+++ src/components/Details.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Details = () => {
+    return (
+        <div>
+            <h1 style={{ color: "blue" }}>This is DETAİLS Page</h1>
+            <p></p>
+        </div>
+    )
+}
+
+export default Details
