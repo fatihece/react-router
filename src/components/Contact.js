03ee1ad7241ec28deacabc0d92eeffbc9d5bff90
fatih <fatihenaz@gmail.com>
2021-09-01 19:12:23 +0300

    useHistory added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import { useHistory } from 'react-router';
 
 const Contact = () => {
+    const history = useHistory();
     return (
         <div>
-            <h1 style={{color:"green"}}>This is CONTACT Page</h1>
+            <h1 style={{ color: "green" }}>This is CONTACT Page</h1>
+            <button onClick={()=> history.push("/")}>Go to HOME</button>
         </div>
     )
 }
 
 export default Contact
+//---------1
+// const Contact = ({history}) => {
+//     return (
+//         <div>
+//             <h1 style={{ color: "green" }}>This is CONTACT Page</h1>
+//             <button onClick={()=> history.push("/")}>Go to HOME</button>
+//         </div>
+//     )
+// }
+
+//---------2
+// const Contact = ({this.props.}) => {
+//     return (
+//         <div>
+//             <h1 style={{ color: "green" }}>This is CONTACT Page</h1>
+//             <button onClick={()=> props.history.push("/")}>Go to HOME</button>
+//         </div>
+//     )
+// }
